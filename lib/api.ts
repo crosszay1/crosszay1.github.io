15891b87dddf8b2056cc36d1cf0e718786f710a4
crosszay1 <Isaiahbfreundlich@Gmail.com>
2026-07-31 21:33:48 -0400

    Don't use our own api to get ip

--- lib/api.ts
+++ lib/api.ts
@@ -1,4 +1,13 @@
 export async function sendRequest(path: string) {
+  if (path === "/api/ip") {
+    const response = await fetch("https://api.ipify.org?format=json");
+    if (!response.ok) {
+      return { ip: "Unknown" };
+    }
+
+    return response.json();
+  }
+
   const response = await fetch(path);
   return response.json();
 }

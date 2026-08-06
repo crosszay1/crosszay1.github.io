24b7ec8c8eb16fb624874187565cb7578d7650e1
crosszay1 <Isaiahbfreundlich@Gmail.com>
2026-08-06 16:47:26 -0400

    Add projects cmd

--- lib/commands.ts
+++ lib/commands.ts
@@ -57,5 +57,16 @@ export const commands: Record<string, (args?: string[]) => string> = {
 
   clear: () => {
     return "__CLEAR__"; // We can detect this and reset the history
-  }
+  },
+
+  projects: () => {
+    return [ // There's gotten be a better way.. there's gotta be
+      "",
+      "You can find an up-to date list of my projects on my GitHub page: https://github.com/crosszay1",
+      "However some major projects that I have worked on include:",
+      " - RathHacksRedCLinux - A custom built linux distribution for the RathHacks NonProfit. Designed to be lightweight, responsive, and easy to use for students, it was shipped to many under-privilleged students who would not otherwise been able to afford a computer.",
+      " - JoshBot - A Discord bot that aims to help bring cybersecurity and OSINT tools to the general public. By replacing a complicated CLI with an easy-to-use Discord bot, JoshBot allows users to easily access and utilize these tools without needing to understand the underlying technology.",
+      "",
+    ].join("\n");
+  },
 };

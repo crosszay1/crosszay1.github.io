9c8bbe47e27a0e5cc8b9726ef5cbbde8f684d6ea
crosszay1 <Isaiahbfreundlich@Gmail.com>
2026-08-22 22:09:43 -0400

    Remove stub, everything works now, now just gotta actually add the files we want

--- lib/commands.ts
+++ lib/commands.ts
@@ -83,7 +83,11 @@ export const commands: Record<string, (args?: string[]) => string> = {
     const path = args[0] ?? "";
     if (fs.isDirValid(path)) return `${path} is a directory: ${path}`;
     if (fs.openFile(path) === null) return `No such file or directory: ${path}`;
-    return `Opening ${path}...`;
+    
+    // Okay now we gotta figure out what the content of the file is. At some point we'll have multiple file types, but for now just txt files
+    const content = fs.readFile(path);
+    if (content === null) return `Error` // Just so typescript stops complaining, but this should never happen since we already checked if the file exists and is a file
+    return content;
   },
 
   projects: () => {

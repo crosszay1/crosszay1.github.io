1e5daf8927ea2c746b0c76ebb42dec85c99bb262
crosszay1 <104384163+crosszay1@users.noreply.github.com>
2026-09-09 17:12:26 +0000

    Add localhost to next.config.ts allowedDevOrigins

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig = {
   images: {
     unoptimized: true, // GitHub Pages doesn't support the default image optimization server
   },
+  allowedDevOrigins: [`127.0.0.1`],
 };
 
 module.exports = nextConfig;
